ba9c5d520b346f5def451c139c64fe1bfbbb77e8
nmorgado <48693922+ngamorg@users.noreply.github.com>
2019-10-24 08:32:22 +0200

    Create Vacc.py

---
+++ code/Vacc.py
@@ -0,0 +1,33 @@
+"""
+
+Authors
+-------
+Vacci-nation group
+Julius Siebenaller, Jannes Hühnerbein, Benjamin Gundersen & Nicolas Antunes Morgado
+2019++
+
+Topic
+-----
+Epidemiology & Immunization
+
+Description
+-----------
+* Insert summary of what the code accomplishes *
+
+"""
+
+# import all required libraries
+import numpy as no
+import matplotlib.pyplot as plt
+
+# define classes & utility functions
+def hello_world():
+"""
+SPECIFICATION: here goes a description of the function/class method should do. It helps both consumers of the function and as a form of documentation.
+PRE: pre-conditions of the function/method. Here the type of the inputs is made explicit, so as to avoid (for example) runtime errors. 
+POST: post-condition; analogous to PRE.
+"""
+    print("Hello, World!")
+
+# program says hello and finishes
+hello_world()
