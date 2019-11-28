f74983dc6c4c91f0064773d23bc3bfbfec605ba2
Benjamin Gundersen <bgunders@student.ethz.ch>
2019-11-28 11:22:19 +0100

    addded distinction between groups in plots

--- code/new_code/main.py
+++ code/new_code/main.py
@@ -1,15 +1,14 @@
 import networkx as nx
 import numpy as np
-import scipy.stats
 import matplotlib.pyplot as plt
-
+from matplotlib.animation import FuncAnimation
 from small_world_network import SmallWorldNetwork
 
 from agent import Agent, Health, Group, GroupBehavior
 
 
 n = 1000
-group_percentages = [0.4, 0.15, 0.3, 0.15]
+group_percentages = [0.6, 0.4]
 k = 4
 change_edge_percentage = 0.2
 alpha = 0.2
@@ -104,49 +103,111 @@ def export(net, agents, v):
         nx.write_gexf(net, "export/export-"+str(v)+".gexf")
 
 
-timesteps = 120
-save_after = 120
+def simulate_export():
+    timesteps = 120
+    save_after = 120
 
-world = setup()
-count_status(agents)
+    count_status(agents)
+    plot = [{} for i in range(timesteps)]
+    plot[0]['sus'] = Agent.num_sus
+    plot[0]['inf'] = Agent.num_inf
+    plot[0]['rec'] = Agent.num_rec
+    plot[0]['vac'] = Agent.num_vac
+    plot[0]['recvac'] = Agent.num_vac + Agent.num_rec
+    nx.write_gexf(world.network, "export/export-groups.gexf")
+    for i in range(timesteps):
+        print("simulated " + str(i))
 
-plot = [{} for i in range(timesteps)]
-plot[0]['sus'] = Agent.num_sus
-plot[0]['inf'] = Agent.num_inf
-plot[0]['rec'] = Agent.num_rec
-plot[0]['vac'] = Agent.num_vac
-plot[0]['recvac'] = Agent.num_vac + Agent.num_rec
+        # save?
+        # if i % save_after == 0:
+        #    export(world.network, agents, i)
+
+        simulate(world, group_behaviours, agents)
+        count_status(agents)
+        # plot
+        plot[i]['sus'] = Agent.num_sus
+        plot[i]['inf'] = Agent.num_inf
+        plot[i]['rec'] = Agent.num_rec
+        plot[i]['vac'] = Agent.num_vac
+        plot[i]['recvac'] = Agent.num_rec + Agent.num_vac
+    fig = plt.figure()
+    ax = fig.add_subplot(111, axisbelow=True)
+    ax.plot(range(timesteps), [elem['sus'] for elem in plot], 'green', lw=1.5, label="Susceptible")
+    ax.plot(range(timesteps), [elem['vac'] for elem in plot], 'blue', lw=1.5, label="Vaccinated")
+    ax.plot(range(timesteps), [elem['rec'] for elem in plot], 'orange', lw=1.5, label="Recovered")
+    ax.plot(range(timesteps), [elem['inf'] for elem in plot], 'red', lw=1.5, label="Infected")
+    ax.plot(range(timesteps), [elem['recvac'] for elem in plot], 'darkgrey', lw=1.5, label="Recovered or Vaccinated")
+    ax.set_ylim(0, world.network.number_of_nodes())
+    ax.set_xlim(0, timesteps)
+    plt.show()
 
 
-nx.write_gexf(world.network, "export/export-groups.gexf")
+def health2num(health):
+    if health == agent._health.SUSCEPTIBLE:
+        return 0
+    if health == agent._health.INFECTED:
+        return 1
+    if health == agent._health.RECOVERED:
+        return 2
+    if health == agent._health.VACCINATED:
+        return 3
 
-for i in range(timesteps):
-    print("simulated " + str(i))
 
-    # save?
-    #if i % save_after == 0:
-    #    export(world.network, agents, i)
+def generate_health_list(agents):
+    """
+    iterate through all agends and generate a list infected where agent[i].infected == 1 <=> infected[i] == 1, else 0
+    """
+    infected = []
+
+    for i in range(0, len(agents)):
+        infected.append(health2num(agents[i]._health))
+    return infected
+
+
+def split_groups(nodes, group_list, health_color):
+    health_color0 = []
+    health_color1 = []
+
+    nodes0 = []
+    nodes1 = []
+    for i in nodes:
+        if group_list[i]%2 == 0:
+            health_color0.append(health_color[i])
+            nodes0.append(i)
+        else:
+            health_color1.append(health_color[i])
+            nodes1.append(i)
+
+    return nodes0, nodes1, health_color0, health_color1
+
+
+def time_stamp(iteration):
+    print(iteration)
 
     simulate(world, group_behaviours, agents)
-    count_status(agents)
-    # plot
-    plot[i]['sus'] = Agent.num_sus
-    plot[i]['inf'] = Agent.num_inf
-    plot[i]['rec'] = Agent.num_rec
-    plot[i]['vac'] = Agent.num_vac
-    plot[i]['recvac'] = Agent.num_rec + Agent.num_vac
+    fig.clf()
+
+    n0, n1, h0, h1  = split_groups(world.network, world.group_colors, generate_health_list(agents))
+
+    edges = nx.draw_networkx_edges(world.network, pos,width = 0.1, edgelist=world.network.edges)
+    
+    
+    nodes0 = nx.draw_networkx_nodes(n0, pos,node_size=7, node_shape='^',  nodelist=n0,
+                                   node_color=h0, label='Trusters')
+    nodes1 = nx.draw_networkx_nodes(n1, pos, node_size=7, nodelist=n1, node_color=h1, label='Skepticals')
+    plt.legend(scatterpoints=1) 
+    return world.network
 
 
-fig = plt.figure()
-ax = fig.add_subplot(111, axisbelow=True)
-ax.plot(range(timesteps), [elem['sus'] for elem in plot], 'green', lw=1.5, label="Susceptible")
-ax.plot(range(timesteps), [elem['vac'] for elem in plot], 'blue', lw=1.5, label="Vaccinated")
-ax.plot(range(timesteps), [elem['rec'] for elem in plot], 'orange', lw=1.5, label="Recovered")
-ax.plot(range(timesteps), [elem['inf'] for elem in plot], 'red', lw=1.5, label="Infected")
-ax.plot(range(timesteps), [elem['recvac'] for elem in plot], 'darkgrey', lw=1.5, label="Recovered or Vaccinated")
-ax.set_ylim(0, world.network.number_of_nodes())
-ax.set_xlim(0, timesteps)
-plt.show()
+def simulate_animation():
+    ani = FuncAnimation(fig, time_stamp, frames=100, interval=100)
+    ani.save('network.mp4', fps=4, extra_args=['-vcodec', 'libx264'])
 
-print("Done")
+
+world = setup()
+fig = plt.figure(dpi = 300)
+pos = nx.spring_layout(world.network)
+
+simulate_animation()
+
 exit()
