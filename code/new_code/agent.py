3819f86285e1a0d8295659fd5047b9d4fed2b356
bgunders <56644686+bgunders@users.noreply.github.com>
2019-12-02 16:43:36 +0100

    Update agent.py

--- code/new_code/agent.py
+++ code/new_code/agent.py
@@ -19,11 +19,11 @@ class GroupBehavior():
     def __init__(self, type):
         self.type = type
         if type == Group.SKEPTICAL:
-            self.Ci = 1.0
-            self.Cv = 1.0
+            self.Ci = 1
+            self.Cv = 0.05
         elif type == Group.TRUSTER:
-            self.Ci = 2.0
-            self.Cv = 1.0
+            self.Ci = 1
+            self.Cv = 0.01
 
 
 class Agent(object):
@@ -31,14 +31,18 @@ class Agent(object):
     Agent class able to play the vaccination game
     """
 
+    sv = 0
+    snv = 0
     # Static members
     r = 0.01
 
     # min 3-4
-    T = 30  # (monthly)
-    beta = 0.1  # 0.0 <= beta <= 1.0
+    T = 10  # (monthly)  # 0.0 <= beta <= 1.0
+    beta = 0.1
 
     num_vac = 0
+    num_vacT = 0
+    num_vacS = 0
     num_sus = 0
     num_inf = 0
     num_rec = 0
@@ -90,6 +94,7 @@ class Agent(object):
         self._health = health
         self._group = group
         self._lambda_k = 0.0
+        self._lambda_rel_k = 0.0
         self._gamma_k = self.estimate_gamma()
 
         self._health_next = None
@@ -116,7 +121,7 @@ class Agent(object):
         self.look(depth_neighbors, agents)
         dec = np.random.uniform(0.0, 1.0)
         if dec < 1/self.T and not self._health_next == Health.INFECTED:
-            self.act(group_behaviours, neighbors, agents)
+            self.act(group_behaviours)
 
     def look(self, neighbors, agents):
         """
@@ -126,8 +131,10 @@ class Agent(object):
 
         # : neighborhood[:] is an array containing the health states ('SUSCEPTIBLE', 'INFECTED',
         # 'RECOVERED', 'VACCINATED') of the neighbors of self
+        total = 0
         count_infected = 0
         for i in neighbors:
+            total += 1
             if agents[i].get_health_status() == Health.INFECTED:
                 count_infected += 1
 
@@ -136,88 +143,9 @@ class Agent(object):
 
         # Update probability of infection
         self._lambda_k = self.estimate_lambda(count_infected)
+        self._lambda_rel_k = Agent.beta * count_infected / total
 
-    def act(self, group_behaviours, neighbors, agents):
-        '''
-
-        When an agent vaccinates it transitions according to the rule: S -> V (recovered agents
-        are assumed to become fully immunized, so there is no increased payoff on vaccination).
-        The agent is assumed to be a rational utility maximizer with only one possible decision
-        per action, that is, to vaccinate with probability *pvacc* every *T* time steps if it
-        is currently in the susceptible state *S* (otherwise no decision is taken).
-        The utility function takes the following form, following the Bellman equation for a
-        discrete-time Markov process:
-
-        U_k(x) = sum_{t = 0}^{t = T} x_k(t) * f_k(t) / (1 + r)^t                                             (1)
-
-        Where:
-            x_k(t) : health state vector of agent k
-            x_k(t = 0) = [ 1 - pvacc, 0, 0, pvacc ] (initial health state vector of agent k)
-            f_k(t) : payoff vector of agent k
-            f_k(t = 0) = [ 0, 0, 0, -Cv_k ]         # instantaneous payoff gains associated with each transition
-            f_k(t > 0) = [ 0, -Ci_k, 0, 0 ]         # gains per unit of time for residents of each state
-            Cv_k : cost of vaccination for agent k
-            Ci_k : cost of infection for agent k
-            r : discount rate
-            T : event duration (i.e. before a new utility estimation)
-
-        The agent decided wether to vaccinate or not by solving the optimization problem:
-
-            max pvacc {U(x)}
-
-        Additionally, the evolution of the system is given by a stochastic matrix Q:
-
-        x_k(t_{n + 1}) = Q_k * x_k(t_{n})                                                                        (2)
-
-        Q_k = [ p_k(S -> S), p_k(I -> S), p_k(R -> S), p_k(V -> S) ]
-              [ p_k(S -> I), ...                     , p_k(V -> I) ]
-              [ ...                                   ...          ]
-              [ p_k(S -> V), ...                     , p_k(V -> V) ]
-
-        And in particular:
-
-        Q_k = [ 1 - lambda_k,           0, 0, 0 ]
-              [ lambda_k    , 1 - gamma_k, 0, 0 ]
-              [            0,     gamma_k, 1, 0 ]
-              [            0,           0, 0, 1 ]
-
-        Where:
-            lambda_k: probability of infection for a susceptible agent k (estimated using
-            local prevalence of infection, times some contact infection probability)
-            gamma_k: probability of recovery of infected agent k
-
-        Equation (1) can be rewritten using equation (2):
-
-        U_k(x) = ( f_k(t = 0) + sum_{t = 1}^{t = T} Q^t * f_k(t) / (1 + r)^t ) * x_k(0)                      (3)
-
-        In order to compute the power Q^t, the matrix can be diagonalized:
-
-        Q_k = P_k^-1 * L_k * P_k
-
-        So that:
-
-        U_k(x) = ( f_k(t = 0) + P_k^-1 * sum_{t = 1}^{t = T} (L_k / (1 + r))^t * P_k * f_k(t) ) * x_k(0)     (4)
-
-        Finally, performing the required computations:
-
-        U_k(x) = -pvacc * Cv_k - (1 - pvacc) * Cnotv_k                                                       (5)
-
-        Where:
-            Cnotv_k = Ci_k * (lambda_k / (gamma_k - lambda_k)) *
-            (sum_{t = 1}^{t = T} ((1 - lambda_k) / (1 + r)))^t - sum_{t = 1}^{t = T}((1 - gamma_k) / (1 + r))^t)
-
-        Equation (5) is linear in pvacc, so it only has one global maximum depending on the
-        relative cost of each decision. In particular:
-
-            * Cv_k > Cnotv_k -> pvacc = 0 (not vaccinating is the preferred strategy)
-            * Cnotv_k > Cv_k -> pvacc = 1 (vaccinating is the preferred strategy)
-            * Cv_k = Cnotv_k -> 0 <= pvacc <= 1 (the agent is indiferent towards vaccinating or not)
-
-        Ref.:
-        * Shim et al. (2012) - A game dynamic model for vaccine skeptics and vaccine believers:
-        Measles as an example
-
-        '''
+    def act(self, group_behaviours):
         # If agent is already vaccinated or recovered, no action takes place
 
         # Look around and update infection probability (lambda)
@@ -230,6 +158,7 @@ class Agent(object):
 
         lambda_k = self._lambda_k  # review of lambda required -> 1 - beta^(infected neighbors)
         gamma_k = self._gamma_k
+        lambda_rel_k = self._lambda_rel_k
 
         for i in range(0, len(group_behaviours)):
             if self._group == group_behaviours[i].type:
@@ -238,10 +167,10 @@ class Agent(object):
 
         sum = [0.0, 0.0]
         for t in range(1, T + 1):
-            sum[0] += ((1 - lambda_k) / (1 + r)) ** t
+            sum[0] += ((1 - lambda_rel_k) / (1 + r)) ** t
             sum[1] += ((1 - gamma_k) / (1 + r)) ** t
 
-        Cnotv_k = Ci_k * (lambda_k / (gamma_k - lambda_k)) * (sum[0] - sum[1])
+        Cnotv_k = Ci_k * (lambda_rel_k / (gamma_k - lambda_rel_k)) * (sum[0] - sum[1])
 
         # Get pvacc
         if Cv_k > Cnotv_k:
@@ -250,6 +179,8 @@ class Agent(object):
             pvacc = 1.0
         else:
             pvacc = np.random.uniform(0.0, 1.0)
+        if lambda_rel_k > 0:
+            print(self._group, pvacc, Cv_k, Cnotv_k, "s0 / 1", sum[0], sum[1], "g, l", gamma_k, lambda_rel_k)
 
         # Update health state based on pvacc
         p = np.random.uniform(0.0, 1.0)
@@ -280,15 +211,15 @@ class Agent(object):
         0 < age < 100 years, and maximum probability of recovery at an
         age = 25 years
         '''
-        maxGamma = 0.1
+        maxGamma = 0.05
+        maxGamma += 0.00000019121986216
+        return maxGamma
         """
-        Not yet age-dependent
         if self._age < 25.0:
             return maxGamma * (1.0 - abs(self._age - 25.0) / 25.0)
         else:
             return maxGamma * (1.0 - abs(self._age - 25.0) / (100 - 25.0))
         """
-        return maxGamma
 
     def get_health_status(self):
         return self._health
